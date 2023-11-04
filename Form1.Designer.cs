94f84a5171b1e4671f5edf37fd49ca6f3016f0cd
Матей Ачев <stu2201261042@uni-plovdiv.bg>
2023-11-04 17:36:19 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(601, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(598, 491);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(52, 183);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(74, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 147);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter scaling percentage";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(52, 229);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 36);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "pick image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.gif;*.bmp;*.tif;*.tiff;*.ico;*.svg";
+            this.openFileDialog1.Title = "Select an image file";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(55, 288);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(134, 48);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "go!";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(55, 361);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(213, 288);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(283, 119);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "GO FASTER !!!!!";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(337, 115);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(192, 146);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(58, 390);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1247, 527);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Bitmap DownscaleImage(Bitmap originalImage)
+        {
+            double scalingFactor = double.Parse(textBox1.Text);
+            label1.Text = "Enter scaling percentage";
+            if (scalingFactor > 0)
+            {
+
+                int newWidth = (int)(originalImage.Width * scalingFactor / 100);
+                int newHeight = (int)(originalImage.Height * scalingFactor / 100);
+
+                Bitmap scaledImage = new Bitmap(newWidth, newHeight);
+
+                for (int x = 0; x < newWidth; x++)
+                {
+                    for (int y = 0; y < newHeight; y++)
+                    {
+                        int originalX = (int)(x * (originalImage.Width - 1) / (newWidth - 1));
+                        int originalY = (int)(y * (originalImage.Height - 1) / (newHeight - 1));
+
+                        Color originalColor = originalImage.GetPixel(originalX, originalY);
+                        scaledImage.SetPixel(x, y, originalColor);
+                    }
+                }
+                pictureBox1.Image = scaledImage;
+                return scaledImage;
+            }
+            else { label1.Text = "no"; return originalImage; }
+        }
+        //**********************************************************************************************************************
+        // plan is to divide the image first
+        // then work simultaniously on them with a modded version of the simple downscaler
+        // and finally put the image slices back together
+        public Bitmap TDownscaleImage(Bitmap originalImage)
+            
+        {
+            double scalingFactor = double.Parse(textBox1.Text);
+            label1.Text = "Enter scaling percentage";
+            if (scalingFactor > 0)
+            {
+
+                int newWidth = (int)(originalImage.Width * scalingFactor / 100);
+                int newHeight = (int)(originalImage.Height * scalingFactor / 100);
+
+                Bitmap scaledImage = new Bitmap(newWidth, newHeight);
+
+                for (int x = 0; x < newWidth; x++)
+                {
+                    for (int y = 0; y < newHeight; y++)
+                    {
+                        int originalX = (int)(x * (originalImage.Width - 1) / (newWidth - 1));
+                        int originalY = (int)(y * (originalImage.Height - 1) / (newHeight - 1));
+
+                        Color originalColor = originalImage.GetPixel(originalX, originalY);
+                        scaledImage.SetPixel(x, y, originalColor);
+                    }
+                }
+                //pictureBox1.Image = scaledImage;
+                return scaledImage;
+            }
+            else { label1.Text = "no"; }return originalImage; }
+
+        public Bitmap CombineImages(Bitmap[] imageSections)
+        {
+            if (imageSections.Length < 4 || imageSections.Any(section => section == null))
+            {
+                return null;
+            }
+
+            int totalWidth = imageSections[0].Width + imageSections[1].Width + imageSections[2].Width + imageSections[3].Width;
+            int totalHeight = imageSections[0].Height + imageSections[1].Height + imageSections[2].Height + imageSections[3].Height;
+
+            
+            Bitmap combinedImage = new Bitmap(totalWidth, totalHeight);
+
+            using (Graphics g = Graphics.FromImage(combinedImage))
+            {
+                // narejdane
+                g.DrawImage(imageSections[0], new Point(0,0));
+                g.DrawImage(imageSections[1], new Point(imageSections[0].Width,0));
+                g.DrawImage(imageSections[2], new Point(imageSections[1].Width*2,0));
+                g.DrawImage(imageSections[3], new Point(imageSections[2].Width*3,0));
+            }
+
+            return combinedImage;
+        }
+        private const int ThreadCount = 4;
+
+
+        public void DivideImage(Bitmap originalImage)
+        {
+            
+            int sectionWidth = originalImage.Width / ThreadCount;
+            int sectionHeight = originalImage.Height / ThreadCount;
+
+            Bitmap[] imageSections = new Bitmap[ThreadCount];
+            Thread[] workers = new Thread[ThreadCount];
+
+            for (int i = 0; i < ThreadCount; i++)
+            {
+                int startX = i * sectionWidth;
+                int startY = 0;
+                int width = sectionWidth;
+                int height = originalImage.Height;
+
+                
+                if (i == ThreadCount - 1)
+                {
+                    width = originalImage.Width - startX;
+                }
+
+                
+                imageSections[i] = originalImage.Clone(new Rectangle(startX, startY, width, height), originalImage.PixelFormat);
+
+                
+                int sectionIndex = i;
+                workers[i] = new Thread(() =>
+                {
+                    imageSections[sectionIndex] = TDownscaleImage(imageSections[sectionIndex]);
+                });
+                workers[i].Start();
+            }
+
+            
+            foreach (Thread worker in workers)
+            {
+                worker.Join();
+            }
+
+            
+            pictureBox1.Image = CombineImages(imageSections);
+        }
+//********************************************************************************************************************
+        private void label1_Click(object sender, EventArgs e)
+        {
+            // bruh
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string imgFileName =  openFileDialog1.FileName;
+                pictureBox1.Image=Image.FromFile(imgFileName);
+            }
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            // ....ok
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Stopwatch time= new Stopwatch();
+            time.Start();
+            Bitmap image = new Bitmap(pictureBox1.Image);
+            DownscaleImage(image);
+            time.Stop();
+            label2.Text= "Normal exec time: " + time.ElapsedMilliseconds;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            // AAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Stopwatch time = new Stopwatch();
+            time.Start();
+            Bitmap image = new Bitmap(pictureBox1.Image);
+            DivideImage(image);
+            time.Stop();
+            label3.Text = "Cool exec time: " + time.ElapsedMilliseconds;
+        }
+    }
+}
